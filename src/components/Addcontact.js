b5db9eb4a858768d262fa91a4838aad7af3650aa
Tejas Musale <tejasmusale99@gmail.com>
2024-05-04 20:46:34 +0530

    create form in addContact compo

--- src/components/Addcontact.js
+++ src/components/Addcontact.js
@@ -1,5 +1,16 @@
 const Addcontact = () =>{
-    return
+    return(
+        <div className="formheader">
+            <div className="add-contact">Addd Contact</div>
+            <form>
+                <label>Name: </label>
+                <input type="text" placeholder="Enter Your Name" />
+                <label>Email: </label>
+                <input type="email" placeholder="Enter Your Email" />
+            </form>
+            <button>Add Contact</button>
+        </div>
+    )
 }
 
 export default Addcontact;
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import '../App.css';
+import Addcontact from './Addcontact';
 import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Addcontact />
     </div>
   );
 }
